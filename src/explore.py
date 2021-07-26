5be3a1a493bfb6345e7bb0db8ca13a954f6813e9
Christopher Logan Ortiz <christopher.logan.ortiz@gmail.com>
2021-07-26 18:32:58 -0500

    Created initial function to resample street sweeping citations for exploration and charts.

--- src/explore.py
+++ src/explore.py
@@ -2,4 +2,46 @@
 import pandas as pd
 import plotly.express as px
 
+############################### Global Variables #########################################
+
+
+############################# Exploration Datasets #######################################
+
+def street_sweep(data):
+    '''
+    Set the index of the street sweeping dataset to 'issue_time' for resampling.
+    
+    Parameters
+    ----------
+    data : pandas.core.DataFrame
+        dataset of steet sweeping citations.
+    
+    Returns
+    -------
+    dataset : pandas.core.DataFrame
+        dataset of street sweeping citations indexed by issue date.
+    '''
+
+    dataset = data.set_index('issue_time')
+    return dataset
+
+
+def resample_period(data, period='D'):
+    '''
+    Calculate citation revenue for the specified time period.
+    
+    Parameters
+    ----------
+    data : pandas.core.DataFrame
+        Dataset of street sweeping citations
+
+    Returns
+    -------
+    dataset : pandas.core.DataFrame
+        Resample dataset of street sweeping citation revenue
+    
+    '''
+    dataset = data.resample(rule=period)['fine_amount'].sum()
+    return dataset
+
 ############################### Visualizations ###########################################
